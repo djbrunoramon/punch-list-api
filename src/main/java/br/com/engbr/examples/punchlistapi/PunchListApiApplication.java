16d605a24b989f267ddd687f5d95963e07a72325
Bruno Ramon <brmachado_5@hotmail.com>
2022-03-07 23:26:23 -0300

    First commit

---
+++ src/main/java/br/com/engbr/examples/punchlistapi/PunchListApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.engbr.examples.punchlistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunchListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunchListApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/engbr/examples/punchlistapi/PunchListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.engbr.examples.punchlistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunchListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
